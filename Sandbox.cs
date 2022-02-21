f1b57725180b5e75f15d4cd4ed1c1400b7eeeca2
Pablo <pablo.catanor@gmail.com>
2022-02-20 23:16:56 -0500

    Final
    Final

--- Sandbox.cs
+++ Sandbox.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+
 
 namespace TestProject1
 {
@@ -6,8 +9,25 @@ namespace TestProject1
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            Console.WriteLine("{0}: {1}");
             Console.WriteLine("Here you can write console prints to test your implementation outside the testing environment");
+
+            Dictionary<int, string> result = new Dictionary<int, string>();
+
+            result.Add(2, "Two");
+            result.Add(4, "Two");
+            result.Add(5, "Five");
+            result.Add(7, "Seven");
+            result.Add(10, "Two");
+
+            var items = from pair in result
+                        orderby pair.Key descending
+                        select pair;
+
+            foreach (var value in items)
+            {
+                Console.WriteLine(value);
+            }
         }
     }
 }
--- TestMethods.cs
+++ TestMethods.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System;
+using System.Linq;
 
 namespace TestProject1
 {
@@ -15,33 +17,112 @@ namespace TestProject1
 
         internal static Stack<int> GetNextGreaterValue(Stack<int> sourceStack)
         {
-            Stack<int> result = null;
+            int[] list = new int[sourceStack.Count];
+            
+            Stack<int> result = new Stack<int>();
+            
+            int a;
+            foreach (int item in sourceStack)
+            {
+                a = item;
+                foreach (int item_ in sourceStack)
+                { 
+
+                    if (a <= item_)
+                    {
+                        result.Push(-1);
+                    }
+                    else
+                    {
+                        a = item_;
+                        result.Push(item_);
+                    }
+                }
+            }
 
             return result;
         }
 
         internal static Dictionary<int, EValueType> FillDictionaryFromSource(int[] sourceArr)
         {
-            Dictionary<int, EValueType> result = null;
+            Dictionary<int, EValueType> result = new Dictionary<int, EValueType>();
+          
+            for (int i = 0; i < sourceArr.Length; i++)
+            {
+                bool correcto = false;                
+                int dividiendo = sourceArr[i];
+                Math.DivRem(dividiendo, 2, out int residuo2);
+                Math.DivRem(dividiendo, 3, out int residuo3);
+                Math.DivRem(dividiendo, 5, out int residuo5);
+                Math.DivRem(dividiendo, 7, out int residuo7);
 
+                if (residuo2 == 0)
+                {
+                    result.Add(sourceArr[i], EValueType.Two);
+                    correcto = true;
+                }
+                else if (residuo3 == 0 && correcto == false)
+                {
+                    result.Add(sourceArr[i], EValueType.Three);
+                    correcto = true;
+                }
+                else if (residuo5 == 0 && correcto == false)
+                {
+                    result.Add(sourceArr[i], EValueType.Five);
+                    correcto = true;
+                }
+                else if (residuo7 == 0 && correcto == false)
+                {
+                    result.Add(sourceArr[i], EValueType.Seven);
+                    correcto = true;
+                }
+                else
+                {
+                    result.Add(sourceArr[i], EValueType.Prime);
+                }                                
+            }
             return result;
-        }
+
+        }       
 
         internal static int CountDictionaryRegistriesWithValueType(Dictionary<int, EValueType> sourceDict, EValueType type)
         {
-            return 0;
+            int num = 0;
+
+            EValueType[] Array = sourceDict.Values.ToArray();
+
+            for(int i = 0; i < Array.Length; i++)
+            {
+                if(Array[i] == type)
+                {
+                    num++;
+                }
+            }
+
+            return num;
         }
 
         internal static Dictionary<int, EValueType> SortDictionaryRegistries(Dictionary<int, EValueType> sourceDict)
         {
-            Dictionary<int, EValueType> result = null;
+            Dictionary<int, EValueType> result = new Dictionary<int, EValueType>();
+
+             var items = from pair in sourceDict
+                     orderby pair.Key descending
+                     select pair;
+
+            foreach (KeyValuePair<int, EValueType> pair in items)
+            {
+                result.Add(pair.Key, pair.Value);
+            }
 
             return result;
         }
 
         internal static Queue<Ticket>[] ClassifyTickets(List<Ticket> sourceList)
         {
-            Queue<Ticket>[] result = null;
+            Queue<Ticket>[] result = new Queue<Ticket>[3];
+
+
 
             return result;
         }
@@ -50,7 +131,9 @@ namespace TestProject1
         {
             bool result = false;
 
-            return result;
-        }        
+
+
+             return result;
+        }
     }
 }
